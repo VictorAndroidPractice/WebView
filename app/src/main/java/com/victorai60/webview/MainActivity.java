44e4007c4cc8ba368fd36d11ca71c6bbe514e1d4
郑松 <zheng.song@menpuji.com>
2017-03-30 11:50:01 +0800

    init

---
+++ app/src/main/java/com/victorai60/webview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.victorai60.webview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tencent.smtt.sdk.WebSettings;
+import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        webView = (WebView) findViewById(R.id.web_view);
+        assert webView != null;
+        webView.loadUrl("https://app.menpuji.com/pos/index.html");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+}
